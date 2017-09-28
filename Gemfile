38788d2e7ce03ea62ab5c4044adf5c0012e8d81b
Robin Daugherty <robin@robindaugherty.net>
2017-09-28 17:56:30 -0400

    Use Redis for the cache

--- Gemfile
+++ Gemfile
@@ -73,6 +73,9 @@ gem 'cancancan'
 # Pagination
 gem 'kaminari'
 
+# Cache in Redis
+gem 'redis-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_store, ENV['REDIS_URL'], { expires_in: 1.hour }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
