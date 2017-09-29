3520f90df79a3337b2c865fd1b35e9588b7d7a4b
Robin Daugherty <robin@robindaugherty.net>
2017-09-29 00:19:40 -0400

    Spec to cover new repos from api response

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -4,7 +4,7 @@ class GithubRepo < Repo
     new do |repo|
       repo.name = data.full_name
       repo.provider_uid_or_url = data.id
-      repo.provider_data = data
+      repo.provider_data = data.to_h
     end
   end
 
--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -7,20 +7,43 @@ RSpec.describe GithubRepo, type: :model do
     subject { described_class.new_from_api(repository) }
     let(:api_data) { json_fixture('github/repositories') }
     let!(:api_request) {
-      stub_request(:get, "https://api.github.com/user/repos?access_token=12345")
+      stub_request(:get, "https://api.github.com/user/repos?access_token=#{access_token}")
         .to_return(body: api_data, status: 200)
     }
-    let(:github_client) { Github.new(oauth_token: '12345') }
-    let(:repository) { github_client.repositories.list.first }
+    let(:access_token) { "12345" }
+    let(:github_client) { Github.new(oauth_token: access_token) }
 
-    it { is_expected.to be_kind_of(described_class) }
-    it 'assigns the name from the API' do
-      expect(subject.name).to eq("octocat/Hello-World")
+    context 'with one repo from a list of repositories' do
+      let(:repository) { github_client.repositories.list.first }
+
+      it { is_expected.to be_kind_of(described_class) }
+      it 'assigns the name from the API' do
+        expect(subject.name).to eq("octocat/Hello-World")
+      end
+      it 'assigns data the API response' do
+        expect(subject.provider_data.name).to eq("Hello-World")
+        expect(subject.provider_data.fork).to be_truthy
+        expect(subject.provider_data.owner.login).to eq("octocat")
+      end
     end
-    it 'assigns data the API response' do
-      expect(subject.provider_data.name).to eq("Hello-World")
-      expect(subject.provider_data.fork).to be_truthy
-      expect(subject.provider_data.owner.login).to eq("octocat")
+
+    context 'with one repo from a direct lookup', vcr: { cassette_name: 'github_repo' } do
+      let(:access_token) { Rails.configuration.x.github.test_auth_token }
+      let(:repository) {
+        github_client.repositories.get(owner_name, repo_name)
+      }
+      let(:owner_name) { Rails.configuration.x.github.test_repo_owner_name }
+      let(:repo_name) { Rails.configuration.x.github.test_repo_name }
+
+      it { is_expected.to be_kind_of(described_class) }
+      it 'assigns the name from the API' do
+        expect(subject.name).to eq("#{owner_name}/#{repo_name}")
+      end
+      it 'assigns data the API response' do
+        expect(subject.provider_data.name).to eq(repo_name)
+        expect(subject.provider_data.fork).to be_falsey
+        expect(subject.provider_data.owner.login).to eq(owner_name)
+      end
     end
   end
 end
