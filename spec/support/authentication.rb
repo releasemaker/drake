9df2cc20e9a8434d8d59256d41adcd1a3d697335
Robin Daugherty <robin@robindaugherty.net>
2017-10-18 13:14:42 -0400

    Redirect to sign-in when not authenticated

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples :authenticated_endpoint do
   context 'without logging in' do
     it 'redirects to authentication' do
-      expect(do_the_thing).to redirect_to(root_path)
+      expect(do_the_thing).to redirect_to(sign_in_path)
     end
   end
 end
