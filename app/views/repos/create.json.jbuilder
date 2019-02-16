6fec7b32eabecd6dd36b604fc67f0ed3c4739299
Robin Daugherty <robin@robindaugherty.net>
2019-02-15 19:20:43 -0500

    Remove old Repos controller

--- app/views/repos/create.json.jbuilder
+++
@@ -1,5 +0,0 @@
-json.id @repo.id
-json.enabled @repo.enabled
-json.name @repo.name
-json.provider_uid @repo.provider_uid_or_url
-json.url friendly_repo_url(@repo)
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,6 @@ Rails.application.routes.draw do
   resource :github_webhooks, only: :create, defaults: { formats: :json }
 
   get :dashboard, to: redirect('/repos')
-  # resources :repos
-  # resources :repos, controller: 'repos', type: 'GithubRepo', as: :github_repos
-  # get '/gh/:owner/:repo' => 'repos#show_by_name', as: :github_repo_by_name
 
   namespace :api, default: { format: 'json' } do
     get 'availableRepos', to: 'available_repos#index'
