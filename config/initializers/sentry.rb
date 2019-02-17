11ff89bfbc0bde4694b533a80eb0f3a74f6f8333
Robin Daugherty <robin@robindaugherty.net>
2019-02-17 00:40:49 -0500

    Fix Raven-Ruby release name in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,23 +1,12 @@
+require_relative 'revision'
+
 if ENV['SENTRY_DSN']
   Raven.configure do |config|
-    # config.dsn = Rails.configuration.sentry.dsn
-    # config.environments = %w(production staging development)
-    # config.current_environment = Rails.env
-    # config.excluded_exceptions = %w(
+    # config.excluded_exceptions = %w[
     #   ActionController::RoutingError
     #   SidekiqWorkerFailure
     #   InsufficientPermissionError
-    # )
-
-    begin
-      File.open(Rails.root.join('REVISION')) do |revision_file|
-        if Rails.env.production?
-          config.release = revision_file.read.strip.split("\n").first
-        else
-          config.release = revision_file.read.strip.split("\n").second
-        end
-      end
-    rescue Errno::ENOENT
-    end
+    # ]
+    config.release = APPLICATION_REVISION
   end
 end
