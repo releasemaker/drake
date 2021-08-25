65d8aa13ccc20e7be0393a1030560689ae53fc8c
Robin Daugherty <robin@robindaugherty.net>
2021-08-25 14:46:31 -0400

    Fix use of instance variable in helper

--- app/controllers/concerns/displays_navigation.rb
+++ app/controllers/concerns/displays_navigation.rb
@@ -3,7 +3,15 @@
 module DisplaysNavigation
   extend ActiveSupport::Concern
 
+  included do
+    helper_method :navigation_disabled?
+  end
+
   def disable_navigation
     @disable_navigation = true
+  end
+
+  def navigation_disabled?
+    @disable_navigation || false
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def show_navigation?
-    logged_in? && !(@disable_navigation || false)
+    logged_in? && !navigation_disabled?
   end
 
   def github_auth_path
