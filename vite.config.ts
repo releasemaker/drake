8bcf691794d879779a7eb0c0795424aa083e5222
Robin Daugherty <robin@robindaugherty.net>
2021-08-25 09:40:14 -0400

    Support https for vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import { resolve } from 'path'
 import { readFile } from 'fs/promises'
+import { readFileSync } from 'fs'
 
-export default defineConfig({
+var config = {
   plugins: [
     RubyPlugin(),
   ],
@@ -36,4 +37,15 @@ export default defineConfig({
       'components': resolve(__dirname, 'app/frontend/components'),
     },
   },
-})
+}
+
+if (process.env.VITESERVER_HTTPS_KEY_PATH !== undefined) {
+  config.server = {
+    https: {
+      key: readFileSync(process.env.VITESERVER_HTTPS_KEY_PATH),
+      cert: readFileSync(process.env.VITESERVER_HTTPS_CERT_PATH)
+    }
+  }
+}
+
+export default defineConfig(config)
